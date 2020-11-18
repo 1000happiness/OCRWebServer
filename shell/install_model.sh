f5f5c9fdd0bd48f2f3a956f263f3f90bb6e5dcfe
1000happiness <1000happiness@sjtu.edu.cn>
2020-11-18 16:54:43 +0800

    ok

--- shell/install_model.sh
+++ shell/install_model.sh
@@ -8,7 +8,3 @@ wget https://paddleocr.bj.bcebos.com/20-09-22/cls/ch_ppocr_mobile_v1.1_cls_infer
 tar -xf ch_ppocr_mobile_v1.1_cls_infer.tar
 wget https://paddleocr.bj.bcebos.com/20-09-22/mobile/rec/ch_ppocr_mobile_v1.1_rec_infer.tar
 tar -xf ch_ppocr_mobile_v1.1_rec_infer.tar
-cd model
-rm *.tar
-cd ..
-mv model ../extension/OCR/model
