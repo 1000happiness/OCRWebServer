42a0000d5bc7f9ea5cbf5f48aeb91bb7819546c5
1000happiness <1000happiness@sjtu.edu.cn>
2020-11-23 17:15:40 +0800

    add run.sh

--- shell/install_model.sh
+++ shell/install_model.sh
@@ -8,3 +8,4 @@ wget https://paddleocr.bj.bcebos.com/20-09-22/cls/ch_ppocr_mobile_v1.1_cls_infer
 tar -xf ch_ppocr_mobile_v1.1_cls_infer.tar
 wget https://paddleocr.bj.bcebos.com/20-09-22/mobile/rec/ch_ppocr_mobile_v1.1_rec_infer.tar
 tar -xf ch_ppocr_mobile_v1.1_rec_infer.tar
+rm *.tar
